e7f559f42c6c3caff39b02171084c5f75d870569
Eric <emei@ucsd.edu>
2026-05-16 16:53:53 -0700

    d

--- map.js
+++ map.js
@@ -2,3 +2,14 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXJpY2ZpcmUyMDAyIiwiYSI6ImNtcDh6Zm5zbzBhZzUyc24xMjU5Y2EybjUifQ.eoFRxS8B7b1esdFM-V099A';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
