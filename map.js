855cdd81bce6dc30e8b2a77536ed11b1cc7faada
Eric <emei@ucsd.edu>
2026-05-16 17:18:05 -0700

    d

--- map.js
+++ map.js
@@ -14,3 +14,28 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+
+map.on('load', () => {
+  console.log('Map loaded successfully');
+
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+});
+
+map.on('error', (e) => {
+  console.error('Mapbox error:', e);
+});
