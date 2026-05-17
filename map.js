d44cbc9b90d016eb8292a2977b4965f431eae081
Eric <emei@ucsd.edu>
2026-05-16 17:02:32 -0700

    d

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
+console.log('map.js is running');
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXJpY2ZpcmUyMDAyIiwiYSI6ImNtcDh6Zm5zbzBhZzUyc24xMjU5Y2EybjUifQ.eoFRxS8B7b1esdFM-V099A';
